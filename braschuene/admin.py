f4bc677d83137671fd07f8933b5816ca02f2e5a6
Julian Bürklin <jb@software-design.de>
2024-04-26 13:13:51 +0200

    adds Debt to django admin

--- braschuene/admin.py
+++ braschuene/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from braschuene.models import Debt
+
 
 # Register your models here.
+@admin.register(Debt)
+class DebtAdmin(admin.ModelAdmin):
+    list_display = ("amount", "date", "get_senders", "get_receivers", "note", "done")
+    list_filter = ("date", "done")
+    search_fields = ("note",)
+    actions = ["mark_as_done", "mark_as_not_done"]
+
+    def mark_as_done(self, request, queryset):
+        queryset.update(done=True)
+
+    def mark_as_not_done(self, request, queryset):
+        queryset.update(done=False)
